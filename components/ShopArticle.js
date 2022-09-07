e3aba7bafbee6b35854d3f4eb74a4c5d82147ae2
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-09-07 12:07:58 +0200

    shop style done

--- components/ShopArticle.js
+++ components/ShopArticle.js
@@ -1,5 +1,6 @@
 import styles from "../styles/ShopArticle.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const ShopArticle = ( { shopArticle } ) => {
 
@@ -9,9 +10,16 @@ const ShopArticle = ( { shopArticle } ) => {
 
   return (
     <article className={styles.shopArticle}>
-        <Image src={plantimageUrl.url} layout="responsive" width={50} height={50} alt={name}/>
+      <div className={styles.articleImage}>
+      <Image src={plantimageUrl.url} layout="fixed" width={200} height={200} alt={name}/>
+
+      </div>
         <h2>{name}</h2>
-        <p>{description}</p>
+        <p className={styles.description}>{description}€</p>
+        <p className={styles.price}>{price}€</p>
+        <div className={styles.viewProductContainer}>
+          <Link href="#">View Product</Link>
+        </div>
     </article>
   )
 }
