03874b836049c8d34d5b2419bcf3e1dcf39a1929
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-09-15 16:29:20 +0200

    carrousel done

--- components/Carrousel.js
+++ components/Carrousel.js
@@ -7,15 +7,34 @@ import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 const Carrousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 5 - 1 ? 0 : currentSlide + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 4 : currentSlide - 1)
+  }
+
+
+  console.log(currentSlide)
 
   return (
     <div className={styles.carrousel}>
       <h2 className="heading">Our Services</h2>
-     <GoChevronRight className={styles.rightArrow}/>
-      {sliderData.map((slide) => {
-        return <Slide key={slide.name} slide={slide} />;
+     <GoChevronRight className={styles.rightArrow} onClick={nextSlide}/>
+      {sliderData.map((slide, index) => {
+
+        return(
+          <div  key={index} className={ index === currentSlide ? 'slide active' : 'slide'}>
+          {
+            index === currentSlide && (
+            <Slide slide={slide} />
+            )
+          }
+          </div>
+        )
       })}
-          <GoChevronLeft className={styles.leftArrow}/>
+          <GoChevronLeft className={styles.leftArrow} onClick={prevSlide}/>
     </div>
   );
 };
