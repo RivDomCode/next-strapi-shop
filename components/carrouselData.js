cb9819f5b66d32fcf03d58cd4b829c4bebc3338d
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-09-16 18:04:45 +0200

    carrousel styled

--- components/carrouselData.js
+++ components/carrouselData.js
@@ -1,17 +1,27 @@
 export const carrouselData = [
     {
-        name:"item1",
-        url: "https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        text:"daçkasdsa asjkdfpoajksdças"
+        name:"Awesome Designs",
+        url: "https://images.unsplash.com/flagged/photo-1552686234-8e47d5602508?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80",
+        text:"We implement your dreamed garden"
     },
     {
-        name:"item2",
-        url: "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80",
-        text:"daçkasdsa asjkdfpoajksdças"
+        name:"Garden Care",
+        url: "https://images.unsplash.com/photo-1586280268958-9483002d016a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1390&q=80",
+        text:"Go and look after your garden"
     },
     {
-        name:"item3",
-        url: "https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        text:"daçkasdsa asjkdfpoajksdças"
+        name:"Quality seeds",
+        url: "https://images.unsplash.com/photo-1457530378978-8bac673b8062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        text:"Best quality selected seeds"
+    },
+    {
+        name:"Plant selection",
+        url: "https://images.unsplash.com/photo-1598002582975-6ea1f15c7027?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        text:"Visit our facilities to check our wide range of varieties"
+    },
+    {
+        name:"Online Shop",
+        url: "https://images.unsplash.com/photo-1525033842647-a956848705f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        text:"Or buy them online in our shop throught this website "
     },
 ]
