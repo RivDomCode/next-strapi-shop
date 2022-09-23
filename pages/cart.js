5c2210eaa919d6d543f02a3177890c82d88b0729
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-09-23 11:18:33 +0200

    reduce to calculate de total price

--- pages/cart.js
+++ pages/cart.js
@@ -1,22 +1,38 @@
 import styles from "../styles/Cart.module.css";
 import Layout from '../components/Layout'
 import CartArticle from "../components/CartArticle";
+import { useEffect, useState } from "react";
 
-const cart = ( {cart, updateQuantity, removeArticle} ) => {
+const Cart = ( {cart, updateQuantity, removeArticle} ) => {
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const totalUpdated = cart.reduce((total, product) => total +(product.quantity * product.price), 0)
+    setTotal(totalUpdated)
+  }, [cart])
+
 
   return (
     <Layout page="shopping-cart">
         <h2 className="heading">Shopping Cart</h2>
         <main className={`${styles.cart} container`}>
-            {
+          <div>
+          {
               cart.map(cartArticle => (
                 <CartArticle key={cartArticle.id} cartArticle={cartArticle} updateQuantity={updateQuantity} removeArticle={removeArticle}/>
                 )
               )
             }
+          </div>
+
+            <div className={styles.order}>
+                <h3>Your Order</h3>
+                <p>Total price: {total}</p>
+            </div>
         </main>
     </Layout>
   )
 }
 
-export default cart
+export default Cart
