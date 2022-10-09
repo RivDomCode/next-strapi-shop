6a6af4fa9b0854a8029de3aec80157f5c2c0e550
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-10-09 18:26:01 +0200

    env changed

--- strapi-plant-shop/config/database.js
+++ strapi-plant-shop/config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-ccu5l7cgqg4a92863sjg-a.frankfurt-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapiplantshop'),
-      user: env('DATABASE_USERNAME', 'pedro'),
-      password: env('DATABASE_PASSWORD', 'xG3p8dS0MLV5ZsZyqwzek1FZtCGPG499'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
